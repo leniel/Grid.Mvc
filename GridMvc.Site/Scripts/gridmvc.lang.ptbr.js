174e1b6a3f328fac480a3e762e448863f0fbb8f8
Luiz Ricardo Abaque Coelho <luizracoelho@gmail.com>
2015-12-11 09:04:37 -0200

    Include Language pt-Br

---
+++ GridMvc.Site/Scripts/gridmvc.lang.ptbr.js
@@ -0,0 +1,24 @@
+﻿/***
+* Grid.Mvc portuguese brazil language (ptbr) http://gridmvc.codeplex.com/
+*/
+window.GridMvc = window.GridMvc || {};
+window.GridMvc.lang = window.GridMvc.lang || {};
+GridMvc.lang.ptbr = {
+    filterTypeLabel: "Tipo: ",
+    filterValueLabel: "Valor :",
+    applyFilterButtonText: "Aplicar",
+    filterSelectTypes: {
+        Equals: "Igual",
+        StartsWith: "Começa com",
+        Contains: "Contém",
+        EndsWith: "Termina com",
+        GreaterThan: "Maior que",
+        LessThan: "Menor que",
+        GreaterThanOrEquals: "Maior ou igual à",
+        LessThanOrEquals: "Menor ou igual à",
+    },
+    code: 'pt-Br',
+    boolTrueLabel: "Sim",
+    boolFalseLabel: "Não",
+    clearFilterLabel: "Limpar",
+};
